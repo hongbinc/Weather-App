1257034d9cd59d6991c3ad34691f957a7b29545c
hongbinc <hongbinchb@gmail.com>
2017-01-07 17:17:42 -0500

    Structuring the weather app

--- app/app.js
+++ app/app.js
@@ -1,13 +1,17 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+// Main module of the weather application
+angular
+  .module('myApp', [
+    'ngRoute'
+  ])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'main.html',
+        controller: 'mainController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/controllers/mainController.js
@@ -0,0 +1,6 @@
+'use strick';
+
+angular.module('myApp')
+    .controller('mainController',function($scope){
+        $scope.message = "Hey angular";
+});
