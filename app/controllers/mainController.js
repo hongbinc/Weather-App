491b1e7af72389bcb6d62fcf5eb822243d78215a
hongbinc <hongbinchb@gmail.com>
2017-01-09 15:51:56 -0500

    Set up current weather data

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -2,12 +2,48 @@
 
 angular.module('myApp')
     .controller('mainController',function($scope, $log, weatherDataService, sharedWeatherData){
+        
+        // Set default zipcode to New York
         $scope.zipCode = "10001";
 
+        $scope.currentWeather = {}; 
 
+        function prepareCurrentWeatherData(data){
+            setLocation(data);
+            setCurrentDescription(data);
+            setTemp(data);
+        }
+
+
+        function setTemp(data){
+            var currentTemp = data.item.condition.temp;
+            var todayHighTemp = data.item.forecast[0].high;
+            var todayLowTemp = data.item.forecast[0].low;
+            
+            $scope.currentWeather.currentTemp = currentTemp;
+            $scope.currentWeather.todayHighTemp = todayHighTemp;
+            $scope.currentWeather.todayLowTemp = todayLowTemp;
+        }
+
+        function setCurrentDescription(data){
+            var currentDescription = data.item.condition.text;
+            $scope.currentWeather.currentDescription = currentDescription;
+        }
+
+        function setLocation(data){
+            var location = data.location;
+            var country = location.country === "United States" ? "USA" : location.country;
+            var address = location.city + ", " + location.region +", " + country;
+            
+            $scope.currentWeather.location = address;
+        }
+        
         var onSearchComplete = function(data){
             if(data.query.results){
-                sharedWeatherData.setWeatherData(data.query.results.channel);
+                var data = data.query.results.channel;
+                sharedWeatherData.setWeatherData(data);
+
+                prepareCurrentWeatherData(data);
             }
             else{
                 // Yahoo API sometimes return null, so keep calling until success.
